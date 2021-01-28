1bdcdb2140348b0883dc1c15b151311889d4f23e
1 <mar982@mail.ru>
2021-01-28 16:46:42 +0300

    less12 task1 complete Remake

--- src/main/java/part1/lesson12/Task1.java
+++ src/main/java/part1/lesson12/Task1.java
@@ -1,26 +1,17 @@
 
 package part1.lesson12;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Task1 {
 
     public static Integer[] modiefedCreatedNumbers(int arrSize, int minValue, int maxValue) {
-        /**
-         * преобразование класса CreateNumArr в стрим Random из дз 8
-         */
-        int[] array = new Random ().ints ( arrSize, minValue, maxValue ).toArray ();
-        /**
-         * преобразование int в Integer для дальнейшего использования в программе
-         */
-        Integer[] workingNum = new Integer[array.length];
-        Arrays.setAll ( workingNum, i -> array[i] );
-
-        for ( Integer integer : workingNum ) {
-            System.out.print ( integer+", " );
-        }
-        return workingNum;
+        return
+                new Random ()
+                        .ints ( arrSize, minValue, maxValue )
+                        .peek ( System.out::println )
+                        .boxed ()
+                        .toArray ( Integer[]::new );
     }
 
     public static void main(String[] args) {
